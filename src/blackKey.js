c3c12733e42ea01048af6a5bdb1b547ecee2068e
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-07-29 13:56:00 -0600

    Configured ESLint for project, fixed react context import errors, set up useContext for managing global synth reference

--- src/blackKey.js
+++ src/blackKey.js
@@ -1,23 +1,23 @@
-import { useRef } from 'react';
-import './styles/blackKey.css';
+import { useRef } from 'react'
+import './styles/blackKey.css'
 
 const BlackKey = () => {
-  const backKeyMasterInsetShadow = useRef(null);
-  const blackKeyConnector = useRef(null);
-  const blackKeyBottom = useRef(null);
+  const backKeyMasterInsetShadow = useRef(null)
+  const blackKeyConnector = useRef(null)
+  const blackKeyBottom = useRef(null)
 
   const keyDown = () => {
     backKeyMasterInsetShadow.current.className =
-      'backKeyMasterInsetShadowActive';
-    blackKeyConnector.current.className = 'blackKeyConnectorDown';
-    blackKeyBottom.current.className = 'blackKeyBottomDown';
-  };
+      'backKeyMasterInsetShadowActive'
+    blackKeyConnector.current.className = 'blackKeyConnectorDown'
+    blackKeyBottom.current.className = 'blackKeyBottomDown'
+  }
 
   const keyUp = () => {
-    backKeyMasterInsetShadow.current.className = 'backKeyMasterInsetShadow';
-    blackKeyConnector.current.className = 'blackKeyConnectorUp';
-    blackKeyBottom.current.className = 'blackKeyBottomUp';
-  };
+    backKeyMasterInsetShadow.current.className = 'backKeyMasterInsetShadow'
+    blackKeyConnector.current.className = 'blackKeyConnectorUp'
+    blackKeyBottom.current.className = 'blackKeyBottomUp'
+  }
 
   return (
     <div className="blackKeyMaster" onMouseDown={keyDown} onMouseUp={keyUp}>
@@ -35,7 +35,7 @@ const BlackKey = () => {
       />
       <div className="blackKeyConnectorUp" ref={blackKeyConnector} />
     </div>
-  );
-};
+  )
+}
 
-export default BlackKey;
+export default BlackKey
